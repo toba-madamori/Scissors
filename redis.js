c4e742ba78c6533868186797cd945993b497f91b
Toba <tobajamezmadamori@gmail.com>
2023-06-25 22:36:13 +0100

    modified redis configuration

--- redis.js
+++ redis.js
@@ -1,11 +1,12 @@
 const { createClient } = require("redis")
 
 const redisClient = createClient({
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    }
+    // password: process.env.REDIS_PASSWORD,
+    // socket: {
+    //     host: process.env.REDIS_HOST,
+    //     port: process.env.REDIS_PORT
+    // }
+    url: process.env.REDIS_URL_EXTERNAL
 });
 
 // Log redis errors
